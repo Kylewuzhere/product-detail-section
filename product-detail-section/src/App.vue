f8ba16bedf32e5ec1538f39b4f65b1803890ce04
kylewuzhere <kyvasquez1026@gmail.com>
2024-05-07 15:01:06 +1200

    Add ShoppingCart to App header

--- product-detail-section/src/App.vue
+++ product-detail-section/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import ShoppingCart from './components/ShoppingCart.vue';
 </script>
 
 <template>
   <div class="bg-white sm:w-3/4 w-full mx-auto">
     <header class="flex items-center justify-between bg-[#F6F6F7] h-7 px-4 mt-4 sm:pe-32">
       <div />
-
+      <ShoppingCart />
     </header>
   </div>
   <RouterView />
