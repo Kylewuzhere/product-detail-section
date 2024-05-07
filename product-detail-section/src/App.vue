8c5b2f5a82ee48bfe15fd659047282f41c151c3d
kylewuzhere <kyvasquez1026@gmail.com>
2024-05-07 13:32:30 +1200

    create App page

--- product-detail-section/src/App.vue
+++ product-detail-section/src/App.vue
@@ -3,7 +3,12 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="bg-white sm:w-3/4 w-full mx-auto">
+    <header class="flex items-center justify-between bg-[#F6F6F7] h-7 px-4 mt-4 sm:pe-32">
+      <div />
 
+    </header>
+  </div>
   <RouterView />
 </template>
 
