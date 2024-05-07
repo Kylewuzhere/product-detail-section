0cda7a7ab531e6a711ce15243951e85c30c12d5d
kylewuzhere <kyvasquez1026@gmail.com>
2024-05-07 15:00:33 +1200

    create ShoppingCart component to display cart items

---
+++ product-detail-section/src/components/ShoppingCart.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+import { useCartStore } from '@/stores/cartStore'
+
+const isDropdownOpen = ref(false)
+const cartStore = useCartStore()
+
+const groupBySize = () => {
+  const grouped = {}
+
+  cartStore.cart.forEach((item) => {
+    const key = `${item.title}-${item.size}`
+    if (key in grouped) {
+      grouped[key].quantity++
+    } else {
+      grouped[key] = { ...item, quantity: 1 }
+    }
+  })
+
+  return Object.values(grouped)
+}
+
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value
+}
+</script>
+
+<template>
+  <div class="relative">
+    <button
+      @click="toggleDropdown"
+      class="text-xs text-[#888888] ms-96 sm:ms-60 me-4 p-1 focus:bg-white border-2 border-transparent focus:border-[#222222] focus:text-black"
+    >
+      My Cart ({{ cartStore.cart.length }})
+    </button>
+    <div v-if="isDropdownOpen" class="absolute top-7 left-0 bg-white border-2 w-full px-4">
+      <div v-for="item in groupBySize()" :key="item.title" class="flex gap-6 my-8">
+        <!-- by quantity -->
+        <img src="../assets/images/classic-tee.jpg" alt="classic-tee" class="w-20" />
+        <div>
+          <p>{{ item.title }}</p>
+          <p class="font-bold">{{ item.quantity }}x ${{ item.price }}.00</p>
+          <p>Size: {{ item.size }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>../stores/cartStore
